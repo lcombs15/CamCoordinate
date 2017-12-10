7d0ac3e28ad806ca4aae679c7d4e122cf2b7727c
Lucas Combs <l-combs@hotmail.com>
2017-12-10 17:38:57 -0500

    File list idea

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import java.io.File;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		System.out.println("File list:");
+		File f = new File("C:\\Users\\Lucas\\Dropbox\\Camera Uploads\\");
+		
+		for(File file: f.listFiles()) {
+			System.out.println(file.toString());
+		}
 	}
 
 }
