45def2eabada0608c7c8d3e10fd76086767748ba
Lucas Combs <l-combs@hotmail.com>
2018-03-16 21:56:27 -0400

    Things are happening!
    Main prints out a list of files in my Camera Uploads directory
    + Output has groups based on time differences
    + Output is sorted by date modified ASC.
    + A new group is created any time a difference > 15 minutes occurs
    	- Numbers start over for new group
    
    While the simple report main spits out isn't earth-shattering, it's a
    big step for this program!
    
    More to come!

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.Date;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -21,6 +24,40 @@ public class Main extends Application {
 	}
 	
 	public static void main(String[] args) {
-		launch(args);
+		//launch(args);
+		
+		//Test directory
+		File dir = new File("C:\\Users\\Lucas\\Dropbox\\Camera Uploads\\");
+		
+		//List files in directory & sort them by date last modified
+		File files[] = dir.listFiles();
+		Arrays.sort(files,(a, b) -> {
+			return (new Date(a.lastModified())).compareTo(new Date(b.lastModified()));
+		});
+		
+		
+		Date prev = null;
+		int numInGroup = 1;
+		//Print everything out
+		for(int i = 0; i < files.length; i++) {
+			Date current = new Date(files[i].lastModified());
+						
+			long diffInMilli = -1, diffInMin = -1;
+			if(prev != null) {
+				diffInMilli = current.getTime() - prev.getTime();
+				diffInMin = (diffInMilli / 1000) / 60;
+				 if(diffInMin > 15){
+						System.out.println();
+						numInGroup = 1;
+				}else {
+					numInGroup++;
+				}
+			}
+			
+
+			System.out.println(numInGroup + ".) " + current + " " + diffInMin + "\t" + files[i]);	
+			prev = current;
+		}
 	}
+	
 }
