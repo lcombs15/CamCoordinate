a763102bef4662446389b60e9809be41966c56f5
Lucas Combs <l-combs@hotmail.com>
2018-03-25 13:58:41 -0400

    Formatted code in Eclipse.

--- src/application/Main.java
+++ src/application/Main.java
@@ -17,47 +17,45 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("CamCoordinate");
-			primaryStage.show();			
-		} catch(Exception e) {
+			primaryStage.show();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		//launch(args);
-		
-		//Test directory
+		// launch(args);
+
+		// Test directory
 		File dir = new File("C:\\Users\\Lucas\\Dropbox\\Camera Uploads\\");
-		
-		//List files in directory & sort them by date last modified
+
+		// List files in directory & sort them by date last modified
 		File files[] = dir.listFiles();
-		Arrays.sort(files,(a, b) -> {
+		Arrays.sort(files, (a, b) -> {
 			return (new Date(a.lastModified())).compareTo(new Date(b.lastModified()));
 		});
-		
-		
+
 		Date prev = null;
 		int numInGroup = 1;
-		//Print everything out
-		for(int i = 0; i < files.length; i++) {
+		// Print everything out
+		for (int i = 0; i < files.length; i++) {
 			Date current = new Date(files[i].lastModified());
-						
+
 			long diffInMilli = -1, diffInMin = -1;
-			if(prev != null) {
+			if (prev != null) {
 				diffInMilli = current.getTime() - prev.getTime();
 				diffInMin = (diffInMilli / 1000) / 60;
-				 if(diffInMin > 15){
-						System.out.println();
-						numInGroup = 1;
-				}else {
+				if (diffInMin > 15) {
+					System.out.println();
+					numInGroup = 1;
+				} else {
 					numInGroup++;
 				}
 			}
-			
 
-			System.out.println(numInGroup + ".) " + current + " " + diffInMin + "\t" + files[i]);	
+			System.out.println(numInGroup + ".) " + current + " " + diffInMin + "\t" + files[i]);
 			prev = current;
 		}
 	}
-	
+
 }
