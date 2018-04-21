46d76675a1a49a53258129fd8e4a812de4e9397e
Lucas Combs <l-combs@hotmail.com>
2018-04-20 21:34:30 -0400

    Cleaning up.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,5 @@
 package application;
 
-import java.io.File;
-import java.util.Arrays;
-import java.util.Date;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
@@ -25,14 +21,7 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {
 		//launch(args);
-		File f = new File("/home/lucas/Desktop/file.txt");
-		System.out.println(f.isDirectory());
-		System.out.println(f.getPath());
-
-		
-		f = f.getParentFile();
-		System.out.println(f.isDirectory());
-		System.out.println(f.getPath());
+		System.exit(0);
 	}
 	
 }
