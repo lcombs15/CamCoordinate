00ca423107ff9ce60ef442c00eb545a7133d6138
Lucas Combs <l-combs@hotmail.com>
2018-04-20 22:13:40 -0400

    Comments & formatting

--- src/Photos/Photo.java
+++ src/Photos/Photo.java
@@ -1,28 +1,34 @@
+/**
+ * Photo.java
+ * The Photo class is meant to give a wrapper for a Photo's respective File object.
+ * The biggest benefit of this class is the compareTo method for Comparable
+ */
 package Photos;
+
 import java.io.File;
 import java.util.Date;
 
-public class Photo implements Comparable<Photo>{
-	
+public class Photo implements Comparable<Photo> {
+
 	private File f;
-	
+
 	public Photo(String path) {
 		f = new File(path);
 	}
-	
+
 	public Photo(File f) {
 		this.f = f;
 	}
-	
+
 	public File getFile() {
 		return this.f;
 	}
-	
+
 	@Override
 	public int compareTo(Photo p) {
 		return (new Date(this.f.lastModified())).compareTo(new Date(p.getFile().lastModified()));
 	}
-	
+
 	@Override
 	public String toString() {
 		return this.f.getPath();
