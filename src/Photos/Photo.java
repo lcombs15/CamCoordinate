0d96b8307756fa9df2b5b1e2d6e2cc797efa66a3
Lucas Combs <l-combs@hotmail.com>
2018-04-20 21:33:54 -0400

    Added toString & File Constructor

--- src/Photos/Photo.java
+++ src/Photos/Photo.java
@@ -10,12 +10,21 @@ public class Photo implements Comparable<Photo>{
 		f = new File(path);
 	}
 	
+	public Photo(File f) {
+		this.f = f;
+	}
+	
 	public File getFile() {
 		return this.f;
 	}
-
+	
 	@Override
 	public int compareTo(Photo p) {
 		return (new Date(this.f.lastModified())).compareTo(new Date(p.getFile().lastModified()));
-	}	
+	}
+	
+	@Override
+	public String toString() {
+		return this.f.getPath();
+	}
 }
