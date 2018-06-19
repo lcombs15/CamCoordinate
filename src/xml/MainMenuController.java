ba0a23b7d4cf5ee4fc16a6ea3b7554111b243857
Lucas Combs <l-combs@hotmail.com>
2018-06-18 22:04:10 -0400

    Parse directory menu option working

--- src/xml/MainMenuController.java
+++ src/xml/MainMenuController.java
@@ -1,26 +1,46 @@
 package xml;
 
+import java.io.File;
+
 import com.sun.glass.ui.MenuItem;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
+import photos.Photo;
+import photos.PhotoGroup;
 
 public class MainMenuController {
+
+	// Compiler will initialize these from FXML layout
 	@FXML
 	private MenuItem FileCloseMenuItem;
-	
-	private Stage stage;
-	
-	
 	@FXML
-    public void closeEventHandler(ActionEvent e) {
-        stage.close();
-    }
-	
+	private TextArea messageArea;
+
+	// Need reference passed from Main to access stage
+	private Stage stage;
+
+	@FXML
+	public void close(ActionEvent e) {
+		stage.close();
+	}
+
 	public void setStage(Stage stage) {
-        this.stage = stage;
-    }
+		this.stage = stage;
+	}
+
+	@FXML
+	public void parseNewDirectory(ActionEvent e) {
+		// Prompt user to choose directory
+		File f = new DirectoryChooser().showDialog(null).getAbsoluteFile();
+
+		// Print found photos to message area
+		for (PhotoGroup g : PhotoGroup.searchDirectory(f.getPath(), false)) {
+			for (Photo p : g.getPhotos())
+				messageArea.appendText("\n" + p.toString());
+		}
+	}
 }
-
-
