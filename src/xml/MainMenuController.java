dee81e697544b0dcf0dd1dee53f4e2424fd35b97
Lucas Combs <l-combs@hotmail.com>
2018-06-23 16:46:16 -0400

    Started on group display FXML!

--- src/xml/MainMenuController.java
+++ src/xml/MainMenuController.java
@@ -1,12 +1,17 @@
 package xml;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import com.sun.glass.ui.MenuItem;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 import photos.Photo;
@@ -19,6 +24,8 @@ public class MainMenuController {
 	private MenuItem FileCloseMenuItem;
 	@FXML
 	private TextArea messageArea;
+	@FXML
+	private GridPane gridArea;
 
 	// Need reference passed from Main to access stage
 	private Stage stage;
@@ -37,10 +44,27 @@ public class MainMenuController {
 		// Prompt user to choose directory
 		File f = new DirectoryChooser().showDialog(null).getAbsoluteFile();
 
-		// Print found photos to message area
-		for (PhotoGroup g : PhotoGroup.searchDirectory(f.getPath(), false)) {
-			for (Photo p : g.getPhotos())
-				messageArea.appendText("\n" + p.toString());
+		PhotoGroupController controller = new PhotoGroupController();
+		FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("PhotoGroup.fxml"));
+		fxmlLoader.setController(controller);
+
+		try {
+			Node n = fxmlLoader.load();			
+			gridArea.add(n,1,1);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
+		
+		ArrayList<PhotoGroup> groups = PhotoGroup.searchDirectory(f.getPath(), false);
+		
+		// Loop through groups
+		for (PhotoGroup g : groups) {
+			// Print found photos to message area
+			for (Photo p : g.getPhotos())
+				messageArea.appendText("\n" + p.toString());			
+		}
+		
+		controller.setPreviewThumbnail(groups.get(0).getPhotos().get(0).getFile().toURI());
 	}
 }
