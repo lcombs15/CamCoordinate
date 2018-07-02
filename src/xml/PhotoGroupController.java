dc906d42162ae2770dfaff9e449b7abdae2b0383
Lucas Combs <l-combs@hotmail.com>
2018-07-02 19:18:52 -0400

    Re-sizing for PhotoGroup XML improved
     - Looking into different group layout

--- src/xml/PhotoGroupController.java
+++ src/xml/PhotoGroupController.java
@@ -3,9 +3,11 @@ package xml;
 import java.net.URI;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ButtonBar;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
 
 public class PhotoGroupController {
 	// Compiler will initialize these from FXML layout
@@ -15,11 +17,17 @@ public class PhotoGroupController {
 		@FXML
 		private BorderPane mainArea;
 		
+		@FXML
+		private Text groupTitle;
+		
+		@FXML
+		private ButtonBar groupMenu;
+		
 		public void setPreviewThumbnail(URI imgPath) {
 			this.previewThumbnail.setImage(new Image(imgPath.toString()));
-			this.previewThumbnail.fitWidthProperty().bind(mainArea.widthProperty());
-			this.previewThumbnail.fitHeightProperty().bind(mainArea.heightProperty());
-			
 			this.previewThumbnail.setPreserveRatio(true);
+			this.previewThumbnail.fitHeightProperty().bind(mainArea.heightProperty().subtract(groupMenu.heightProperty()));
+			this.previewThumbnail.fitWidthProperty().bind(mainArea.widthProperty());
+			
 		}
 }
