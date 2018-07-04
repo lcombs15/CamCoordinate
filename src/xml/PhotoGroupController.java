25c6183f618b7d1d83a35fc2c77a5c62ccd9c5e2
Lucas Combs <l-combs@hotmail.com>
2018-07-04 10:01:23 -0400

    PhotoGroup XML changed to side-scroll

--- src/xml/PhotoGroupController.java
+++ src/xml/PhotoGroupController.java
@@ -1,33 +1,47 @@
 package xml;
 
-import java.net.URI;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
+import photos.Photo;
+import photos.PhotoGroup;
 
 public class PhotoGroupController {
-	// Compiler will initialize these from FXML layout
+	// Compiler will initialize these from FXML layout		
 		@FXML
-		private ImageView previewThumbnail;
-		
-		@FXML
-		private BorderPane mainArea;
+		private BorderPane controlArea;
 		
 		@FXML
 		private Text groupTitle;
 		
 		@FXML
-		private ButtonBar groupMenu;
+		private ButtonBar menu;
 		
-		public void setPreviewThumbnail(URI imgPath) {
-			this.previewThumbnail.setImage(new Image(imgPath.toString()));
-			this.previewThumbnail.setPreserveRatio(true);
-			this.previewThumbnail.fitHeightProperty().bind(mainArea.heightProperty().subtract(groupMenu.heightProperty()));
-			this.previewThumbnail.fitWidthProperty().bind(mainArea.widthProperty());
+		@FXML
+		private HBox rootHBox;
+		
+		private PhotoGroup photos;
+		
+		public void setPhotoGroup(PhotoGroup photos) {
+			this.photos = photos;
 			
+			
+			
+			for(Photo p: this.photos.getPhotos()) {
+				ImageView photoNode;
+				photoNode = new ImageView();
+				photoNode.setImage(new Image(p.getFile().toURI().toString()));
+				photoNode.setPreserveRatio(true);
+				photoNode.fitHeightProperty().bind(rootHBox.heightProperty());
+				photoNode.fitWidthProperty().bind(rootHBox.widthProperty().multiply(.20));
+				rootHBox.getChildren().add(photoNode);
+			}
+			
+			controlArea.prefHeightProperty().bind(rootHBox.heightProperty());
 		}
+		
 }
